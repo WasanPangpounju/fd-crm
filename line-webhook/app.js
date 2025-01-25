d6f928038b3659da9bb1945c5b9073ae95de02cc
WasanPangpounju <friendlydev.net@gmail.com>
2025-01-26 02:02:36 +0700

    update

--- line-webhook/app.js
+++ line-webhook/app.js
@@ -3,10 +3,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const bodyParser = require('body-parser');
+// const { Client, middleware } = require('@line/bot-sdk');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var webhookRouter = require('./routes/webhook');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+// var webhookRouter = require('./routes/webhook');
 
 const { Client, middleware } = require('@line/bot-sdk');
 
@@ -17,41 +18,62 @@ const config = {
     channelAccessToken: 'zXNSmsTUPcOAEt+c6k/8AAn/16rLmtQVNssnTpsyaKOLNcNmvAlSe9muoU79JOgnfLATOncA3pqRIogRjhEB0teYGsGbSlRlrL4JMSbP4FTRz7DcltsNzErAqqhLziWnMAewkCS10fbB9LzB1eqytwdB04t89/1O/w1cDnyilFU=',
     channelSecret: 'a9c7b16fadc00cb2e4fff146edd0bbfb',
   };
-// Line SDK client
-const client = new Client(config);  
+  // Middleware for verifying LINE signature
+app.use(middleware(config));
 
-// Middleware
-app.use(bodyParser.json());
+// Webhook route
+app.post('/webhook', (req, res) => {
+    const events = req.body.events;
+    const client = new Client(config);
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/webhook', webhookRouter);
-
-// Middleware for Line webhook
-app.post('/webhook', middleware(config), (req, res) => {
-  Promise.all(req.body.events.map(handleEvent))
-    .then(() => res.sendStatus(200))
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
+    events.forEach(event => {
+        if (event.type === 'message' && event.message.type === 'text') {
+            // Reply to user
+            client.replyMessage(event.replyToken, {
+                type: 'text',
+                text: `You said: ${event.message.text}`,
+            });
+        }
     });
+
+    res.sendStatus(200); // Acknowledge the webhook
 });
 
-// Event handler
-function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    return Promise.resolve(null);
-  }
+// // Line SDK client
+// const client = new Client(config);  
 
-  const echo = { type: 'text', text: event.message.text };
+// // Middleware
+// app.use(bodyParser.json());
 
-  return client.replyMessage(event.replyToken, echo);
-}
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+// app.use('/webhook', webhookRouter);
+
+// // Middleware for Line webhook
+// app.post('/webhook', middleware(config), (req, res) => {
+//   Promise.all(req.body.events.map(handleEvent))
+//     .then(() => res.sendStatus(200))
+//     .catch((err) => {
+//       console.error(err);
+//       res.sendStatus(500);
+//     });
+// });
+
+// // Event handler
+// function handleEvent(event) {
+//   if (event.type !== 'message' || event.message.type !== 'text') {
+//     return Promise.resolve(null);
+//   }
+
+//   const echo = { type: 'text', text: event.message.text };
+
+//   return client.replyMessage(event.replyToken, echo);
+// }
 
 module.exports = app;
